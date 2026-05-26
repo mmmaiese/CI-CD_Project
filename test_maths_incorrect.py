c06918473f26134b64533dc632c1d147d30d34b5
mmmaiese <m.maiese612@gmail.com>
2026-05-26 18:15:17 +1000

    Add tests for the add function
    This Python code is incorrect to test the Python CI workflow

---
+++ test_maths_incorrect.py
@@ -0,0 +1,14 @@
+def add(a, b)
+    return a + b
+
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+
+def test_add_negative_numbers():
+    assert add(-1, -1) == -2
+
+
+def test_add_zero():
+    assert add(5, 0) == 5
